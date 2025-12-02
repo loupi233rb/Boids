2879949a6317b21d69a47d2f2ffa820c3f17985d
loupi233 <yaoguang.lei@outlook.com>
2025-12-02 12:31:45 +0800

    change the default parameter values

--- include/bird.h
+++ include/bird.h
@@ -42,16 +42,16 @@ struct vector2{
 
 // 环境设置
 struct EnvSetting{
-    int MX = 800;        // window width
-    int MY = 600;        // window height
+    int MX = 1000;        // window width
+    int MY = 800;        // window height
     int BOUNDARY = 15;
-    int BOUNDARY_FORCE = 100;
-    double DT = 0.02;
-    int LOGIC_FPS = 30;
+    int BOUNDARY_FORCE = 140;
+    double DT = 0.03;
+    int LOGIC_FPS = 5;
     int RENDER_FPS = 60;
     double LOGIC_FPS_UPDATE_SPEED = 0.5;
     double RENDER_FPS_UPDATE_SPEED = 0.2;
-    int BIRD_NUM = 50;
+    int BIRD_NUM = 800;
     bool IS_LEFTBUTTON_DOWN = 0;
     vector2 MOUSE_POSITION = vector2(0,0);
     EnvSetting() = default;
@@ -59,16 +59,16 @@ struct EnvSetting{
 
 // 鸟设置
 struct BirdSetting{
-    float s_w = 1.2;              // separation weight
-    float a_w = 2.0;              // alignment weight
-    float c_w = 1.0;              // cohesion weight
+    float s_w = 1.0;              // separation weight
+    float a_w = 0.8;              // alignment weight
+    float c_w = 0.8;              // cohesion weight
     int s_r = 50;                 // separation range
     int a_r = 200;                // alignment range
-    int c_r = 50;                 // cohesion range
-    double v_max = 100;           // max velocity
-    double f_max = 100;           // max force
-    double f_min = 15;            // min force
-    double mouse_r = 200;         // mouse react range
+    int c_r = 80;                 // cohesion range
+    double v_max = 120;           // max velocity
+    double f_max = 200;           // max force
+    double f_min = 5;             // min force
+    double mouse_r = 250;         // mouse react range
     double noise_w = 1.0;         // noise weight
     std::vector<vector2> shape;
     BirdSetting() = default;
