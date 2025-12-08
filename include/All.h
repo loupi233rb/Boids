3b5e5eb7c7cf928a9a699ef7121623dab5c30a13
loupi233 <yaoguang.lei@outlook.com>
2025-12-08 12:07:17 +0800

    Add Pygame Snake game (src/snake.py), requirements and launcher

---
+++ include/All.h
@@ -0,0 +1,10 @@
+#define ALL_H
+#include <vector>
+#include <thread>
+
+class All
+{
+public:
+    All();
+    ~All();
+};
---
+++ src/test.py
@@ -0,0 +1,10 @@
+"""Launcher for the Snake game.
+
+Run this file to start the Pygame-based 贪吃蛇 (Snake) game.
+"""
+
+from snake import main
+
+
+if __name__ == '__main__':
+	main()
